70c8530b0c360ea2c16822d07eaaa8a0572e2b80
JiMa98 <maji2017@bupt.cn>
2021-12-07 04:23:07 +0800

    view v3

--- usermanager/views.py
+++ usermanager/views.py
@@ -92,3 +92,101 @@ def get_username_from_token(token_string):
     username, _ = get_user_info_from_token(token_string)
     return username
 
+@csrf_exempt
+def sign_up(request):
+    '''sign_up'''
+    if request.method == 'POST':
+        try:
+            json_result = json.loads(request.body)
+            username = json_result.get('username')
+            password = json_result.get('password')
+        except:
+            return gen_response(400, DATA_ERROR_INFO)
+    else:
+        return gen_response(400, TYPE_ERROR_INFO)
+    user = User.objects.filter(username=username).first()
+    if not user:
+        is_manager = False
+        if username == "Anonymous":
+            is_manager = True
+        user = User(username=username, password=password, is_manager=is_manager)
+        try:
+            # 注意在调用full_clean()时Django会自动检测字段的有效性，这个有效性检测包括检测CharField是否满足最大长度限制
+            user.full_clean()
+            # 存入数据库
+            user.save()
+        except ValidationError as e:
+            return gen_response(400, "Validation Error of user: {}".format(e))
+        return gen_response(200, {"is_manager": user.is_manager})
+
+    return gen_response(400, "该用户名已被注册")
+
+
+@csrf_exempt
+def sign_in(request):
+    '''sign_in'''
+    if request.method == 'POST':
+        try:
+            json_result = json.loads(request.body)
+            username = json_result.get('username')
+            password = json_result.get('password')
+        except:
+            return gen_response(400, DATA_ERROR_INFO)
+    else:
+        return gen_response(400, TYPE_ERROR_INFO)
+    user = User.objects.filter(username=username, password=password).first()
+    if not user:
+        return gen_response(400, "用户名或密码错误")
+    return gen_response(200, {"is_manager": user.is_manager}, user.username)
+
+
+@csrf_exempt
+def sign_out(request):
+    '''sign_out'''
+    token_string = request.COOKIES.get("token")
+    Token.objects.filter(token=token_string).delete()
+    return gen_response(200, '')
+
+
+@csrf_exempt
+def modify_password(request):
+    '''modify_password'''
+    token_string = request.COOKIES.get("token")
+    username = get_username_from_token(token_string)
+    if not username:
+        return gen_response(401, SIGN_IN_ERROR_INFO)
+    if request.method == 'POST':
+        try:
+            json_result = json.loads(request.body)
+            password = json_result.get('password')
+            new_password = json_result.get('new_password')
+        except:
+            return gen_response(400, DATA_ERROR_INFO)
+    else:
+        return gen_response(400, TYPE_ERROR_INFO)
+    user = User.objects.filter(username=username, password=password).first()
+    if not user:
+        return gen_response(400, "密码错误,请检查您的输入")
+    User.objects.filter(username=username, password=password).update(password=new_password)
+    return gen_response(200, {}, user.username)
+
+
+@csrf_exempt
+def view_history(request):
+    '''view_history'''
+    token_string = request.COOKIES.get("token")
+    print(request.COOKIES)
+    username = get_username_from_token(token_string)
+    if not username:
+        return gen_response(401, SIGN_IN_ERROR_INFO)
+    history_set = History.objects.filter(username=username)
+    history_info_set = []
+    for history in history_set:
+        doc = Doc.objects.filter(id=history.doc_id).first()
+        if not doc:
+            continue
+        doc_info = {'title': doc.title, 'abstract': doc.abstract, 'context': doc.context}
+        history_info = {'doc_info': doc_info, 'query': history.query, 'answer': history.answer,
+                        'timestamp': history.timestamp}
+        history_info_set.append(history_info)
+    return gen_response(200, history_info_set, username)
