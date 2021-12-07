b6482c5b8d5ce163ecae1df47b0d55a351b43e90
lalalla-hue <56943050+lalalla-hue@users.noreply.github.com>
2021-12-07 23:24:21 +0800

    Add files via upload

--- usermanager/tests.py
+++ usermanager/tests.py
@@ -3,9 +3,10 @@ import json
 import pytest
 from django.test import TestCase
 from django.test.client import RequestFactory
+from django.test import Client
 from .views import sign_in, sign_out, sign_up, view_history, modify_password
 # Create your tests here.
-from django.test import Client
+
 
 TEST_SIGN_UP_URL = "/usermanager/sign_up"
 TEST_SIGN_IN_URL = "/usermanager/sign_in"
