d456cdb0879f55e2dc82c380ffcb7f361c5452aa
Lixy7816 <52002584+Lixy7816@users.noreply.github.com>
2021-12-08 00:31:34 +0800

    delete some useless things and update requirements list

--- BotFront/BotFront/test/unit/specs/ChooseBot.spec.js
+++
@@ -1,9 +0,0 @@
-import ChooseBot from '@/components/ChooseBot'
-import { mount } from '@vue/test-utils'
-
-describe('ChooseBot.vue', () => {
-  it('测试props default', () => {
-    const wrapper = mount(ChooseBot)
-    wrapper.vm.chooseABot(3)
-  })
-})
